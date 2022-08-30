d2856e1ba68d4013b1eadbc98ab0c31ce202820f
Bethelhem Yilma <kukubz42@gmail.com>
2022-08-30 18:41:11 +0200

    local storage fix

--- dream-frontend/src/axiosRequest.js
+++ dream-frontend/src/axiosRequest.js
@@ -3,7 +3,11 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:3005/api/";
 const BASE_URL = "https://dream-interior-design.herokuapp.com/api/"
 
-const user = JSON.parse(localStorage.getItem("persist:root")).loggedUser;
+
+// const user = JSON.parse(localStorage.getItem("persist:root")).loggedUser;
+const user = JSON.parse(localStorage.getItem("persist:root")) !== null && JSON.parse(localStorage.getItem("persist:root")).loggedUser;
+
+console.log(`axios ${user}`)
 const TOKEN = JSON.parse(user)?.accessToken;
 
 console.log(TOKEN);
